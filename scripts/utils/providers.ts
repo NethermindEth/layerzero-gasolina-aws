7871467e89907c68ebe05e43887141dd3ce2dfa0
Adrian Tubal Páez Ruiz <adriantpaez@gmail.com>
2025-06-13 14:42:53 +0200

    Update main branch (#2)
    * chore: update dependencies
    
    * feat: Update Gasolina configuration for mainnet deployment
    
    * refactor: Clean up wallet configuration and enhance Gasolina service task count settings
    
    * feat: update mainnet providers
    
    * feat: add utility to replace API keys in providers.json
    
    * feat: add GitHub Actions workflow for deploying CDK stack with API key replacement
    
    * refactor: adjust minimum and maximum task count settings for Gasolina service
    
    * feat: add EDU mainnet chain
    
    * chore: remove GitHub Actions workflow for deploying CDK stack
    
    * feat: update Abstract mainnet RPC
    
    * feat: update Gasolina API version
    
    * fix: update Abstract RPC
    
    * fix: remove public Ethereum RPCs
    
    * feat: add goat chain
    
    * feat: add solana provider
    
    * feat: update setup configuration
    
    * feat: update to version 1.1.15
    
    * feat: add TON provider and environment configuration
    
    * chore: add TON to available chain names
    
    * feat: add Cronos
    
    * feat: add Japan Open Chain (JOC)
    
    * feat: add cronos
    
    * chore: emove Bera RPC JWT
    
    * chore: bump gasolina app version to 1.1.16
    
    * feat: add story RPC providers
    
    * chore: update RPC provider configurations and add unichain
    
    * feat: add Hyperliquid chain support
    
    * chore: rename cronos provider key to cronosevm
    
    * feat: add Nibiru chain support
    
    * feat: add Concrete mainnet chain
    
    * chore: bump gasolina app version to 1.1.17
    
    * chore: update mainnet provider configurations and add Movement chain support
    
    * chore: update mainnet provider URIs with new API keys and add XDC chain support
    
    * chore: update mainnet provider URIs with new API keys and add PlumePhoenix chain support
    
    * feat: add initia chain
    
    * feat: add Lisk chain
    
    * refac: update Codex's RPC
    
    * chore: bump gasolina app version to 1.1.20
    
    * feat: add animechain and gunz chains, update provider URIs, and enhance rollback functionality for environment variables
    
    * chore: init deploy workflow
    
    * chore: add dependency installation step to deploy workflow
    
    * chore: add AWS CDK diff step to deploy workflow
    
    * chore: bump gasolina app version to 1.1.24 and update available chain names
    
    * chore: update deploy workflow to replace environment variables and enhance provider configurations
    
    * fix: correct path to providers script in deploy workflow
    
    * chore: update deploy workflow to trigger on pull requests and add AWS CDK deploy step
    
    * chore: update deploy workflow to trigger on push to deployment branch
    
    * chore: add AWS CDK synth step to deploy workflow
    
    * chore: add AWS CDK deploy step to deploy workflow

---
+++ scripts/utils/providers.ts
@@ -0,0 +1,87 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as dotenv from 'dotenv';
+
+type EnvVariableConfig = {
+    envVarName: string;
+    anchorString: string;
+};
+
+const replaceEnvVariables = (configs: EnvVariableConfig[]) => {
+    const providersPath = path.join(__dirname, '../../cdk/gasolina/config/providers/mainnet/providers.json');
+
+    try {
+        let providersContent = fs.readFileSync(providersPath, 'utf8');
+
+        configs.forEach(({ envVarName, anchorString }) => {
+            const envVar = process.env[envVarName];
+            if (!envVar) {
+                throw new Error(`${envVarName} environment variable is not set`);
+            }
+            providersContent = providersContent.replace(new RegExp(`\\\${${anchorString}}`, 'g'), envVar);
+            console.log(`Successfully replaced ${envVarName} placeholder in providers.json`);
+        });
+
+        fs.writeFileSync(providersPath, providersContent);
+    } catch (error) {
+        console.error('Error updating providers.json:', error);
+        throw error;
+    }
+};
+
+const rollbackEnvVariables = (configs: EnvVariableConfig[]) => {
+    const providersPath = path.join(__dirname, '../../cdk/gasolina/config/providers/mainnet/providers.json');
+
+    try {
+        let providersContent = fs.readFileSync(providersPath, 'utf8');
+
+        configs.forEach(({ envVarName, anchorString }) => {
+            const envVar = process.env[envVarName];
+            if (!envVar) {
+                console.warn(`${envVarName} environment variable is not set, skipping rollback`);
+                return;
+            }
+            providersContent = providersContent.replace(
+                new RegExp(envVar, 'g'), 
+                `\${${anchorString}}`
+            );
+            console.log(`Successfully rolled back ${envVarName} in providers.json`);
+        });
+
+        fs.writeFileSync(providersPath, providersContent);
+    } catch (error) {
+        console.error('Error rolling back providers.json:', error);
+        throw error;
+    }
+};
+
+dotenv.config();
+
+const configs = [
+    { envVarName: 'INFURA_API_KEY', anchorString: 'infura_api_key' },
+    { envVarName: 'ALCHEMY_API_KEY', anchorString: 'alchemy_api_key' },
+    { envVarName: 'TON_API_KEY', anchorString: 'ton_api_key' },
+    { envVarName: 'MOVEMENT_BEARER_TOKEN', anchorString: 'movement_bearer_token' },
+    { envVarName: 'LENS_TOKEN', anchorString: 'lens_token' },
+    { envVarName: 'ERPC_AUTH_TOKEN', anchorString: 'erpc_auth_token' }
+];
+
+// Get command line argument
+const action = process.argv[2];
+
+if (!action) {
+    console.error('Please provide an action: replace or rollback');
+    process.exit(1);
+}
+
+switch (action.toLowerCase()) {
+    case 'replace':
+        replaceEnvVariables(configs);
+        break;
+    case 'rollback':
+        rollbackEnvVariables(configs);
+        break;
+    default:
+        console.error('Invalid action. Use either "replace" or "rollback"');
+        process.exit(1);
+}
