a9b3e93b04a6b5e07bcfc60f039b72fc533cd895
Haneen Hany <124837763+khalifaa55@users.noreply.github.com>
2025-09-27 18:04:33 +0300

    feat: add support for ethereal chain (#59)
    * feat: add support for ethereal chain
    
    * refac: update name
    
    * feat: version 1.1.36

--- cdk/gasolina/config/index.ts
+++ cdk/gasolina/config/index.ts
@@ -13,10 +13,10 @@ export const CONFIG: {
     // EDIT: aws account number
     '824247070589': {
         gasolinaRepo: 'us-east1-docker.pkg.dev/lz-docker/gasolina/gasolina',
-        appVersion: '1.1.35', // EDIT: version and tag of the gasolina image
+        appVersion: '1.1.36', // EDIT: version and tag of the gasolina image
         projectName: 'layerzero-gasolina', // EDIT: project_name e.g. foobar-gasolina
         environment: 'mainnet', // EDIT: environment e.g. mainnet/testnet
-        availableChainNames: 'abstract,animechain,ape,apexfusionnexus,aptos,arbitrum,astar,aurora,avalanche,base,bb1,bera,bitlayer,blast,bob,botanix,bsc,canto,celo,codex,concrete,conflux,coredao,cronosevm,cronoszkevm,cyber,degen,dexalot,dfk,dos,edu,ethereum,etherlink,fantom,flare,flow,fraxtal,fuse,glue,gnosis,goat,gravity,gunz,harmony,hedera,hemi,hyperliquid,initia,ink,iota,islander,joc,katana,kava,klaytn,lens,lightlink,lisk,manta,mantle,merlin,meter,metis,mode,moonbeam,moonriver,morph,movement,mp1,nibiru,og,opbnb,optimism,orderly,peaq,plasma,plume,plumephoenix,polygon,rarible,real,rootstock,scroll,sei,shimmer,silicon,solana,somnia,soneium,sonic,sophon,story,subtensorevm,sui,superposition,swell,tac,taiko,telos,tomo,ton,tron,unichain,worldchain,xai,xchain,xdc,xlayer,xpla,zircuit,zkconsensys,zkpolygon,zksync,zora',
+        availableChainNames: 'abstract,animechain,ape,apexfusionnexus,aptos,arbitrum,astar,aurora,avalanche,base,bb1,bera,bitlayer,blast,bob,botanix,bsc,canto,celo,codex,concrete,conflux,coredao,cronosevm,cronoszkevm,cyber,degen,dexalot,dfk,dos,edu,ethereal,ethereum,etherlink,fantom,flare,flow,fraxtal,fuse,glue,gnosis,goat,gravity,gunz,harmony,hedera,hemi,hyperliquid,initia,ink,iota,islander,joc,katana,kava,klaytn,lens,lightlink,lisk,manta,mantle,merlin,meter,metis,mode,moonbeam,moonriver,morph,movement,mp1,nibiru,og,opbnb,optimism,orderly,peaq,plasma,plume,plumephoenix,polygon,rarible,real,rootstock,scroll,sei,shimmer,silicon,solana,somnia,soneium,sonic,sophon,story,subtensorevm,sui,superposition,swell,tac,taiko,telos,tomo,ton,tron,unichain,worldchain,xai,xchain,xdc,xlayer,xpla,zircuit,zkconsensys,zkpolygon,zksync,zora',
         signerType: 'KMS', // EDIT: MNEMONIC or KMS
         kmsNumOfSigners: 1, // EDIT: only required if signerType is KMS
         // extraContextRequestUrl: undefined // EDIT: optional
