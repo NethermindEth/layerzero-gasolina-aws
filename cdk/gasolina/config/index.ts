e14761af14040d38bcd9b0582c9dc2451db5e529
Haneen Hany <haneenhany1205@gmail.com>
2025-12-22 21:55:48 +0200

    chore: bumo to v1.1.46

--- cdk/gasolina/config/index.ts
+++ cdk/gasolina/config/index.ts
@@ -13,7 +13,7 @@ export const CONFIG: {
     // EDIT: aws account number
     '824247070589': {
         gasolinaRepo: 'us-east1-docker.pkg.dev/lz-docker/gasolina/gasolina',
-        appVersion: '1.1.45', // EDIT: version and tag of the gasolina image
+        appVersion: '1.1.46', // EDIT: version and tag of the gasolina image
         projectName: 'layerzero-gasolina', // EDIT: project_name e.g. foobar-gasolina
         environment: 'mainnet', // EDIT: environment e.g. mainnet/testnet
         availableChainNames: 'abstract,animechain,ape,apexfusionnexus,aptos,arbitrum,astar,aurora,avalanche,base,bb1,bera,bitlayer,blast,bob,botanix,bsc,camp,canto,celo,citrea,codex,concrete,conflux,coredao,cronosevm,cronoszkevm,cyber,degen,dexalot,dfk,doma,dos,edu,ethereal,ethereum,etherlink,fantom,flare,flow,fraxtal,fuse,gatelayer,glue,gnosis,goat,gravity,gunz,harmony,hedera,hemi,hyperliquid,initia,injectiveevm,ink,iota,iotal1,islander,joc,katana,kava,klaytn,lens,lightlink,lisk,manta,mantle,megaeth,merlin,meter,metis,mode,monad,moonbeam,moonriver,morph,movement,mp1,nexera,nibiru,og,opbnb,openledger,optimism,orderly,peaq,plasma,plume,plumephoenix,polygon,rarible,real,redbelly,rootstock,sagaevm,scroll,sei,shimmer,silicon,solana,somnia,soneium,sonic,sophon,stable,starknet,story,subtensorevm,sui,superposition,swell,tac,taiko,telos,tomo,ton,tron,unichain,worldchain,xai,xchain,xdc,xlayer,xpla,zama,zircuit,zkconsensys,zkpolygon,zksync,zkverify,zora',
