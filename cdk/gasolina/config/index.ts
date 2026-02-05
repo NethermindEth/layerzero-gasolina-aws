ead0c2988dec9e5d2357c888d762965495374bff
Denis Policastro <denis.policastro@gmail.com>
2026-02-04 22:09:59 -0300

    Update appVersion to 1.1.51 in index.ts

--- cdk/gasolina/config/index.ts
+++ cdk/gasolina/config/index.ts
@@ -13,7 +13,7 @@ export const CONFIG: {
     // EDIT: aws account number
     '824247070589': {
         gasolinaRepo: 'us-east1-docker.pkg.dev/lz-docker/gasolina/gasolina',
-        appVersion: '1.1.50', // EDIT: version and tag of the gasolina image
+        appVersion: '1.1.51', // EDIT: version and tag of the gasolina image
         projectName: 'layerzero-gasolina', // EDIT: project_name e.g. foobar-gasolina
         environment: 'mainnet', // EDIT: environment e.g. mainnet/testnet
         availableChainNames: 'abstract,animechain,ape,apexfusionnexus,aptos,arbitrum,astar,aurora,avalanche,base,bera,bitlayer,blast,bob,botanix,bsc,camp,canto,celo,citrea,chiliz,codex,conflux,coredao,cronosevm,cronoszkevm,cyber,degen,dexalot,dfk,doma,dos,edu,ethereal,ethereum,etherlink,fantom,flare,flow,fraxtal,fuse,gatelayer,glue,gnosis,goat,gravity,gunz,harmony,hedera,hemi,horizen,hyperliquid,initia,injectiveevm,ink,iota,iotal1,irys,islander,joc,katana,kava,kite,klaytn,lens,lightlink,lisk,manta,mantle,megaeth,merlin,meter,metis,mode,monad,moonbeam,moonriver,morph,movement,mp1,nexera,nibiru,og,opbnb,openledger,optimism,orderly,peaq,pharos,plasma,plumephoenix,polygon,rarible,redbelly,rise,rootstock,sagaevm,scroll,sei,shimmer,silicon,solana,somnia,soneium,sonic,sophon,stable,starknet,story,subtensorevm,sui,superposition,swell,tac,taiko,telos,tempo,tomo,ton,tron,unichain,worldchain,xai,xchain,xdc,xlayer,xpla,zama,zircuit,zkconsensys,zkpolygon,zksync,zkverify,zora',
